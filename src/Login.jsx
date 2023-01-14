b25a27ab03597ef18fe1e51eecb5e38f583369fc
Kyriakoue89 <122600324+Kyriakoue89@users.noreply.github.com>
2023-01-14 17:33:26 +0200

    styles for login and register form

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from 'react-bootstrap';
+import { Button, Form, Checkbox } from 'react-bootstrap';
 
 export const Login = (props) => {
     const [email, setEmail] = useState ('');
@@ -11,15 +11,28 @@ export const Login = (props) => {
         console.log(email);
     }
     return (
-        <>
-        <form onSubmit={handeSumbit}>
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" placeholder="Email"/>
-            <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)}  type="password" id="password" placeholder="Password"/>
-            <button type="submit">Log In</button>
-        </form>
-            <Button onClick={() => props.onFormSwitch('register')}>Don't have account? Register here</Button>
-            </>
+<div className="container">
+
+    <div className="login-register-container">
+        <div className="row">
+            <div className="col-sm-6"><h3>Log in</h3></div>
+            <div className="col-sm-6 text-end"><a href="" onClick={() => props.onFormSwitch('register')} className="login-regster-link">Already have account? Login here</a></div>
+        </div>
+
+        <Form onSubmit={handeSumbit}>
+            <Form.Group className="mb-2">
+                <Form.Label>Email</Form.Label>
+                <Form.Control size="sm" value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" placeholder="Email"/>
+            </Form.Group>
+            <Form.Group className="mb-2">
+                <Form.Label>Password</Form.Label>
+                <Form.Control size="sm" value={pass} onChange={(e) => setPass(e.target.value)}  type="password" id="password" placeholder="Password"/>
+            </Form.Group>
+            <Form.Group className="mb-2">
+                <Button type="submit" className="btn-success">Login In</Button>
+            </Form.Group>
+		</Form>
+				</div>
+		</div>
     )
 }
